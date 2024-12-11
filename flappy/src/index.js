494970f538a3bf56ee64f6ab18b07e48a66b9e42
Andresl7n7 <eduardocondealvarez2006@gmail.com>
2024-12-11 17:00:16 -0600

    Avance de flappy bird

--- flappy/src/index.js
+++ flappy/src/index.js
@@ -1,28 +1,70 @@
 
-import Phaser from "phaser";
 
-const config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 200 }
+
+
+import Phaser, { Physics } from "phaser";
+
+ const config = {
+    type : Phaser.AUTO,
+    width : 800,
+    height : 600,
+    physics: {
+      default : 'arcade',
+      arcade: {
+        gravity : 200,
+        debug : true
+      }
+
+    },
+    scene: {
+      preload,
+      create,
+      update,
     }
-  },
-  scene: {
-    preload: preload,
-    create: create
+ }
+
+const VELOCIDAD = 300;
+const GRAVEDAD = 1000;
+
+
+let pajarito = null;
+let totalDelta = null;
+
+ function preload() {
+  this.load.image('cielo', 'assets/sky.png')
+  this.load.image('pajarito', 'assets/bird.png')
+ }
+ function create() {
+  this.add.image(0, 0, 'cielo').setOrigin(0,0)
+  pajarito = this.physics.add.sprite(config.width / 10, config.height / 2, 'pajarito')
+
+  this.input.keyboard.on('keydown-W', saltar);
+  // this.input.keyboard.on('keyup-SPACE', caer);
+
+
+  pajarito.body.gravity.y = GRAVEDAD
+  
+  // pajarito.body.gravity.y = 1000;
+  // pajarito.body.velocity.y = 500;
+  
+ }
+
+ function update(time, delta) {
+
+  if(pajarito.y <= config.height - pajarito.height) {
+    pajarito.body.gravity.y = 0
+    pajarito.body.velocity.y = 0
   }
-};
 
-new Phaser.Game(config);
+  
+   
+ }
 
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
-}
+ function saltar() {
+  pajarito.body.velocity.y = -VELOCIDAD
+ }
+//  function caer() {
+//   pajarito.body.gravity.x = 
+//  }
 
-function create () {
-  this.add.image(400, 300, 'sky');
-}
+ new Phaser.Game(config);
