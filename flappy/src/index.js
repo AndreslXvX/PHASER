523baabc4ddb45c0b575186afa5adc59103fa95d
Andresl7n7 <eduardocondealvarez2006@gmail.com>
2024-12-13 17:09:18 -0600

    Optimizacion de codigo

--- flappy/src/index.js
+++ flappy/src/index.js
@@ -11,7 +11,6 @@ import Phaser, { Physics } from "phaser";
     physics: {
       default : 'arcade',
       arcade: {
-        gravity : 200,
         debug : true
       }
 
@@ -23,48 +22,88 @@ import Phaser, { Physics } from "phaser";
     }
  }
 
-const VELOCIDAD = 300;
+ 
+const VELOCIDAD = 200;
 const GRAVEDAD = 1000;
+const posicionInicialPajarito = { x: config.width / 10, y: config.height / 2}
+const rangoDeDistanciaXdeTubitos = 400
 
+const NUMERO_TUBITOS = 8
 
-let pajarito = null;
-let totalDelta = null;
+let tubitos = null
+let pajarito = null
+let velocidadSalto = 400
+let distanciaXdeTubitos = rangoDeDistanciaXdeTubitos
+
 
  function preload() {
   this.load.image('cielo', 'assets/sky.png')
   this.load.image('pajarito', 'assets/bird.png')
+  this.load.image('tubito', 'assets/pipe.png')
  }
+
+ 
  function create() {
+  //objetos
+          //cielito
   this.add.image(0, 0, 'cielo').setOrigin(0,0)
-  pajarito = this.physics.add.sprite(config.width / 10, config.height / 2, 'pajarito')
 
-  this.input.keyboard.on('keydown-W', saltar);
-  // this.input.keyboard.on('keyup-SPACE', caer);
-
-
+  pajarito = this.physics.add.sprite(posicionInicialPajarito.x , posicionInicialPajarito.y,  'pajarito')
   pajarito.body.gravity.y = GRAVEDAD
-  
-  // pajarito.body.gravity.y = 1000;
-  // pajarito.body.velocity.y = 500;
-  
- }
 
- function update(time, delta) {
+  tubitos = this.physics.add.group();
+  
+  //comandos al presionar teclas
+  this.input.keyboard.on('keydown-W', saltar);
+  
 
-  if(pajarito.y <= config.height - pajarito.height) {
-    pajarito.body.gravity.y = 0
-    pajarito.body.velocity.y = 0
+  for (let i = 0 ; i < NUMERO_TUBITOS ; i++) {
+
+  let tubitoSuperior = tubitos.create( 0, 0, 'tubito').setOrigin(0, 1)
+  let tubitoInferior = tubitos.create( 0, 0, 'tubito').setOrigin(0, 0)
+  crearTubitos(tubitoSuperior, tubitoInferior)
+    // debugger
   }
+  
+  tubitos.setVelocityX(-VELOCIDAD)
+  
+  
+
+ }
+
+function crearTubitos(tubitoS, tubitoI) {
 
   
-   
- }
+  distanciaXdeTubitos += rangoDeDistanciaXdeTubitos
+  let rangoDeDistanciaEntreTubitos = [100, 200]
+  let DistanciaEntreTubitos = Phaser.Math.Between(...rangoDeDistanciaEntreTubitos)
+  let posicionDeLosTubitos = Phaser.Math.Between(40, config.height - 40 - DistanciaEntreTubitos )
+
+  tubitoS.y = posicionDeLosTubitos
+  tubitoS.x = distanciaXdeTubitos
+
+  tubitoI.y = tubitoS.y + DistanciaEntreTubitos
+  tubitoI.x = tubitoS.x
+
+  
+
+
+}
+
+ function update() {
+
+  if(pajarito.y >= config.height - pajarito.height || pajarito.y <= 20) {
+    pajarito.body.y = posicionInicialPajarito.y
+    pajarito.body.x = posicionInicialPajarito.x
+    pajarito.body.velocity.y = 0
+  } 
+
+}
 
  function saltar() {
-  pajarito.body.velocity.y = -VELOCIDAD
- }
-//  function caer() {
-//   pajarito.body.gravity.x = 
-//  }
+
+  pajarito.body.velocity.y = -velocidadSalto
+
+ }ww
 
  new Phaser.Game(config);
